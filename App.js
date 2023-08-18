7a0e411e86a756fa86a2fb81f31509159fb50cbb
Naiara Bonfim <108945315+bsnaiara@users.noreply.github.com>
2023-08-17 21:47:36 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TelaPrincipal from './telas/TelaPrincipal';
+import TelaLocaliza from './telas/TelaLocaliza';
+import TelaMeteoro from './telas/TelaMeteoro';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TelaLocaliza/>
       <StatusBar style="auto" />
     </View>
   );
